12dd9598f77b1a11e02c9086ced2d8ad40198f3b
Tsuchihashi-ryo <tsuchihashi-ryo@jbpo.or.jp>
2024-10-09 07:37:26 +0000

    Added new file.

--- pycorn/utils.py
+++ pycorn/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 
-def get_series_from_data(data, data_key_list):
+def get_series_from_data(data, data_key_list,interpolate=True,lightweighting=10):
     try:
         # select the first injection as the injection timestamp
         inject_timestamp = data["Injection"]["data"][-1][0]
@@ -26,5 +26,26 @@ def get_series_from_data(data, data_key_list):
 
     df = df.sort_index()
     df = df.reset_index(names=["mL"])
+
+    if interpolate:
+      df = df.interpolate(method='linear')
+
+    if lightweighting:
+      light_index = np.array(df.index[df.index%10==0])
+
+      # オブジェクトタイプの列を選択
+      object_columns = df.select_dtypes(include=['object']).columns
+
+      # 全てのオブジェクト列でNaNでないインデックスを取得し、1つのリストにまとめる
+      non_nan_indices = sorted(set(df[object_columns].dropna(how="all").index))
+
+      light_index = np.append(light_index,non_nan_indices)
+      light_index = sorted(set(light_index))
+      
+      df = df.loc[light_index]
+      df = df.reset_index(drop=True)
+
+
+
 
     return df
